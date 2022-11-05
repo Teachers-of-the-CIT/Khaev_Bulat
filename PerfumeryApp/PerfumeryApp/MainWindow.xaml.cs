a4b84b5a3e59a92377082cf741c6577bd1b57e0b
Bulat <bulat.khaev@mail.ru>
2022-11-05 12:31:09 +0300

    files

---
+++ PerfumeryApp/PerfumeryApp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PerfumeryApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PerfumeryDBEntities _context = new PerfumeryDBEntities();
+        public Users _user;
+        public MainWindow()
+        {
+            InitializeComponent();
+            goodsDataGrid.ItemsSource = _context.Goods.ToList();
+        }
+        public void getPrivilages()
+        {
+            if (_user != null)
+            {
+                logLabel.Content = $"{_user.Name} ({_user.Login})";
+                if (_user.Role == "Менеджер")
+                {
+
+                }
+                else if (_user.Role == "Администратор")
+                {
+                    ordersBtn.IsEnabled = true;
+                }
+                else
+                    MessageBox.Show("Пожалуйста, авторизуйтесь!");
+
+                loginBtn.IsEnabled = false;
+            }
+        }
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow window = new LoginWindow(this);
+            window.ShowDialog();
+        }
+
+        private void goodBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ordersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OrdersWindow window = new OrdersWindow();
+            window.ShowDialog();
+        }
+    }
+}
---
+++ PerfumeryApp/PerfumeryApp/OrderChangeWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PerfumeryApp
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderChangeWindow.xaml
+    /// </summary>
+    public partial class OrderChangeWindow : Window
+    {
+        OrdersWindow _window;
+        Orders _order;
+        public OrderChangeWindow(OrdersWindow window, Orders order)
+        {
+            InitializeComponent();
+            _window = window;
+            _order = order;
+
+            getDateTb.Text = order.GetDate.ToString();
+            pointCmb.ItemsSource = _window._context.Points.ToList();
+            pointCmb.DisplayMemberPath = "Point";
+            codeTb.Text = order.Code.ToString();
+        }
+
+        private void goBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (getDateTb.Text != "" && pointCmb.Text != "" && codeTb.Text != "" && statusTb.Text != "")
+            {
+                int number;
+                bool success = int.TryParse(codeTb.Text, out number);
+                if (success)
+                {
+                    try
+                    {
+                        _window._context.Orders.Attach(_order);
+                        _order.Code = number;
+                        _order.Status = statusTb.Text;
+                        _order.GetDate = DateTime.Parse(getDateTb.Text);
+                        this.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    
+                }
+                else
+                    Console.WriteLine("Код получения должен содержать только цифры!");
+            }
+            else
+                MessageBox.Show("Заполни все поля.");
+        }
+    }
+}
